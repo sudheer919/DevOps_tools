0e0300032138771a73942124e13c48b3dbddc6c1
sudheer_muthyala <34484660+sudheer919@users.noreply.github.com>
2021-03-29 15:06:34 +0530

    Update install.sh

--- ansible/install.sh
+++ ansible/install.sh
@@ -3,6 +3,6 @@
 sudo yum remove ansible -y 
 rm -rf /usr/lib/python2.7/site-packages/ansible*
 sudo yum install python3 -y
-sudo pip3 install --user ansible
+sudo pip3 install ansible  --user ansible
 
 #

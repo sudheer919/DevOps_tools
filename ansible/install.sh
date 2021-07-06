94b772ebc863e9094206c232187e4a35226df65d
sudheer_muthyala <34484660+sudheer919@users.noreply.github.com>
2021-07-06 09:42:50 +0530

    Update install.sh

--- ansible/install.sh
+++ ansible/install.sh
@@ -5,7 +5,13 @@
 # sudo yum install python3 -y
 # sudo pip3 install ansible  --user ansible
 
-sudo yum install python3 -y
+
+python3 --version
+
+if [ $? -ne 0 ]; then
+    sudo yum install python3 -y   &>>/dev/null
+fi 
+
 curl -s  https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo yum install -y python3*
 python3 get-pip.py

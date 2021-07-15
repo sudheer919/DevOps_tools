6aa3236c80b7249ab2b66f92a2059cfa72c6756f
Your Name <you@example.com>
2021-07-15 15:22:59 +0530

    auto pushing chainges repo

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -4,6 +4,7 @@
 curl -s https://raw.githubusercontent.com/sudheer919/DevOps_tools/main/functions/common_functions.sh -o /tmp/functions.sh
 
 source /tmp/functions.sh
-
+## Checking Root User or not.
 CheckRoot
+
 echo "hello jenkins"

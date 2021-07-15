e86f411011dea0563a9286bf3ba6a7282b5a202a
Your Name <you@example.com>
2021-07-15 14:59:26 +0530

    auto pushing chainges repo

--- functions/common_functions.sh
+++ functions/common_functions.sh
@@ -2,12 +2,13 @@
 
 
 R="\e[31m"
+N="\e[0m"
 ##Checking Root User or not
 
 CheckRoot() {
   RID=$(id -u)
   if [ $RID -ne 0 ]; then
-    echo -e "$R ✗ :: You need to be Root User to perform this command...!\e[0m"
+    echo -e "$R ✗ :: You need to be Root User to perform this command...!$N"
     exit 1
   else
     echo "Helo world"

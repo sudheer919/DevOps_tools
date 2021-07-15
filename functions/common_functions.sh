82791799247c259b2b5c317577f0c69f35aee590
Your Name <you@example.com>
2021-07-15 14:52:07 +0530

    auto pushing chainges repo

---
+++ functions/common_functions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##Checking Root User or not
+
+RID=$(id -u)
+
+if [ $RID -ne 0 ]; then
+
+    echo -e "\e[31mYou need to be Root User to perform this command...!\e[0m"
+    exit 1
+  else
+    echo "Helo world"
+
+fi

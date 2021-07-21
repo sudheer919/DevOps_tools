367e82b13fc3d20de14c948dbc18ae8c1c7ece26
Your Name <you@example.com>
2021-07-21 11:00:42 +0530

    --- Auto-Git Commit ---

--- maven/install.sh
+++ maven/install.sh
@@ -21,7 +21,8 @@ PrintHead() {
 Stat(){
     if [ $1 -ne 0 ] ;then 
         echo -e "${R} $2 UNSUCCESSFUL ${N}"
-    
+    else
+        echo -e ""
 }
 PrintHead "Installing Java"
 yum install java-1.8.0-openjdk-devel unzip  -y &>>/dev/null

e0422dc64dc757b4bab1d255557ec7a460d03ebb
Your Name <you@example.com>
2021-07-21 11:42:05 +0530

    --- Auto-Git Commit ---

--- maven/install.sh
+++ maven/install.sh
@@ -23,6 +23,7 @@ PrintHead() {
 Stat(){
     if [ $1 -ne 0 ] ;then 
         echo -e "${R}✗${N}  $2 ${R}UNSUCCESSFUL${N}"
+        echo -e
         exit
     else
         echo -e "${G}✓${N}  $2 ${G}SUCCESS${N}"

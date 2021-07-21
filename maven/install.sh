a3ae1a3016d99f9605be642b868460938451e026
Your Name <you@example.com>
2021-07-21 11:26:03 +0530

    --- Auto-Git Commit ---

--- maven/install.sh
+++ maven/install.sh
@@ -36,9 +36,7 @@ cd /opt/
 curl -s https://downloads.apache.org/maven/maven-3/${Mvn_Ver}/binaries/apache-maven-${Mvn_Ver}-bin.zip -o /tmp/apache-maven-${Mvn_Ver}-bin.zip
 Stat $? "Downloading maven${Mvn_Ver}"
 
-PrintHead {
-    
-}
+PrintHead "M"
 
 
 

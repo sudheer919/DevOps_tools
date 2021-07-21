ca885d8e3b48bff5ae038a7819dadce508bdc0d5
Your Name <you@example.com>
2021-07-21 11:29:22 +0530

    --- Auto-Git Commit ---

--- maven/install.sh
+++ maven/install.sh
@@ -39,7 +39,7 @@ Stat $? "Downloading maven${Mvn_Ver}"
 PrintHead "Moving mvn command to /bin/mvn"
 unzip -o /tmp/apache-maven-${Mvn_Ver}-bin.zip &>>/dev/null
 mv apache-maven-${Mvn_Ver} maven
-ln -s /opt/maven
+ln -s /opt/maven/bin/mvn  /
 
 
 

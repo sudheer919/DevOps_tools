1f2101088e7a7fdf09e0016a553d35e002642c1b
Your Name <you@example.com>
2021-07-21 11:57:28 +0530

    --- Auto-Git Commit ---

--- maven/install.sh
+++ maven/install.sh
@@ -42,7 +42,7 @@ Stat(){
         echo "----------------------------"
     fi
 }
-#
+# main progream
 Os_checking
 PrintHead "Installing Java"
 yum install java-1.8.0-openjdk-devel unzip  -y &>>$LOG

76c25fb1773f0fb322ea3d51575b9872d8a1ffb6
Your Name <you@example.com>
2021-07-21 14:14:21 +0530

    --- Auto-Git Commit ---

--- sonarqube/install.sh
+++ sonarqube/install.sh
@@ -68,7 +68,7 @@ curl -s -o ${SFILENAME} ${SDURL}
 Stat $? "sonarqub Downlode is ::"
 
 PrintHead "Extracting ${SFILENAME}"
-unzip -o ${SFILENAME} &
+unzip -o ${SFILENAME} &>>$LOG
 mv /home/sonar${SFILENAME}  /home/sonar/sonarqube
 chown sonar:sonar sonarqube -R
 Stat $? "Extracting Sonarqube is ::"

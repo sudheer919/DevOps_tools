6dcaa513fa86785d0a96ac80cec690e7cf6eea48
Your Name <you@example.com>
2021-07-21 14:26:40 +0530

    --- Auto-Git Commit ---

--- sonarqube/install.sh
+++ sonarqube/install.sh
@@ -75,8 +75,6 @@ Stat $? "Extracting Sonarqube is ::"
 
 https://raw.githubusercontent.com/sudheermuthyala/DevOps_tools/main/sonarqube/sonarqube.service >/etc/systemd/system/sonarqube.service
 
-chown sonar:sonar sonarqube -R
-curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/sonarqube/sonar.service 
 systemctl daemon-reload
 systemctl enable sonarqube
 sed -i -e '/^RUN_AS_USER/ d' -e '/#RUN_AS_USER/ a RUN_AS_USER=sonar' /home/sonar/sonarqube/bin/linux-x86-64/sonar.sh

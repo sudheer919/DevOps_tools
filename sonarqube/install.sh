989b55536851b566ffcd97f9d01916dcbe57aeeb
Your Name <you@example.com>
2021-07-21 14:09:00 +0530

    --- Auto-Git Commit ---

--- sonarqube/install.sh
+++ sonarqube/install.sh
@@ -63,6 +63,7 @@ id -u sonar &>>/dev/null || useradd sonar
 Stat $? "sonar User Creation Is ::"
 
 PrintHead "Downloading Sonarqube.zip"
+
 curl -s -o ${SFILENAME} ${SDURL} 
 Stat $? "sonarqub Downlode is ::"
 

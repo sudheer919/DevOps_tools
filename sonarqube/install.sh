54f21a42409cccf07c8988f309b03fe6293c9b0b
Your Name <you@example.com>
2021-07-21 19:13:05 +0530

    --- Auto-Git Commit ---

--- sonarqube/install.sh
+++ sonarqube/install.sh
@@ -16,7 +16,7 @@ SDIRNAME=$(echo ${SFILENAME} |sed -e 's/.zip/ /')
 com
 
 $(curl -s https://binaries.sonarsource.com/Distribution/sonarqube/ | grep '.zip<' | tail -1 | sed -e 's/</ /g'  | xargs -n1 | grep ^href | sed -e 's/href=sonarqube-//' -e 's/.zip>//')
-
+URL="https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-${VERSION}.zip"
 curl -s -o ${SFILENAME} ${SDURL} 
 
 if [ $LID -ne 0 ] ;then

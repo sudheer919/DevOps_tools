b231b37da88179dd737a5a909907d2cb0ee02174
MuthyalaSudheer <sudheerlikeu@gmail.com>
2021-07-27 15:44:27 +0530

    Update install.sh

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -5,7 +5,7 @@ if [ $(id -u) -ne 0 ]; then
   echo -e "\e[31mYou should run this script as root user or sudo user\e[0m"
   exit 1
 fi
-URL="curl -s https://nodejs.org/en/download/ | xargs -n1 | grep 'linux-x64.tar.xz' | sed -e 's|=| |g' -e 's|>| |g' | xargs -n1| grep ^https"
+URL="curl -s https://nodejs.org/en/download/ | xargs -n1 | grep 'linux-x64.tar.xz' | sed -e 's|=| |g' -e 's|>| |g' | xargs -n1 | grep ^https"
 FILENAME=$(echo $URL | awk -F '/' '{print $NF}')
 FOLDER_NAME=$(echo $FILENAME | sed -e 's|.tar.xz| |')
 

f6aa7d4c2a4e880d929262adb619c2a4cbba926c
MuthyalaSudheer <sudheerlikeu@gmail.com>
2021-07-28 13:30:21 +0530

    Update install.sh

--- sonar-scanner/install.sh
+++ sonar-scanner/install.sh
@@ -8,3 +8,6 @@ rm -rf sonar
 mv sonar-scanner-cli-4.6.2.2472-linux sonar
 
 ln -s /opt/sonar/bin/sonar-scanner  /bin/sonar-scanner
+
+curl -s https://raw.githubusercontent.com/sudheermuthyala/DevOps_tools/main/sonar-scanner/sonar-quality-gate.sh >/bin/sonar-quality-gate.sh
+chmod +x /bin/sonar-quality-gate.sh

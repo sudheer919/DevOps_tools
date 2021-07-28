914b595408fa7340f508daf0fe6b7057a1df4632
MuthyalaSudheer <sudheerlikeu@gmail.com>
2021-07-28 13:26:57 +0530

    Create sonar-quality-gate.sh
    check sonar quality status

---
+++ sonar-scanner/sonar-quality-gate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+# This is Scrip is to check Sonar Quality Gates Status is "OK" or "ERROR"
+USERNAME=$1
+PASSWORD=$2
+HOSTIP=$3
+KEY=$4
+STATUS=$(curl -u "$USERNAME:$PASSWORD" "http://${HOSTIP}:9000/api/qualitygates/project_status?projectKey=${KEY}" | jq  '.projectStatus.status' | xargs)
+
+if [ "$STATES" == "OK" ]; then
+  exit 0
+ else
+  exit 1
+fi
+
+
+# USERNAME=admin
+# PASSWORD=sonar123
+# HOSTIP=100.25.77.85
+# KEY=cart (component_name)

0c83f0500435245f9e82d3171b5d16507e05e54c
Sudheer <sudheerlikeu@gmail.com>
2021-09-17 08:05:56 +0530

    auto pushing chainges repo

---
+++ promotheus-server/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Installing Promotheus Server
+
+#Checking Root user
+R="\e[31m"
+UID=$(id -u)
+if [ "$UID" -ne 0 ]; then
+    echo -e "\e[31m✗  Your must be a root user to perform this command..\e[0m"
+    exit 1
+fi
+
+#Checking Promotheus Directory If Exist removing That directory
+if [ -d /opt/prometheus ] ; then
+   rm -rf /opt/prometheus
+
+fi
+
+
+
+URL=$(curl -s -L   https://prometheus.io/download/ | grep tar  | grep prometheus- | grep linux-amd64 |sed -e "s|>| |g" -e "s|<| |g" -e "s|\"| |g" | xargs -n1 | grep ^https | tail -1)
+
+echo $URL

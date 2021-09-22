4f3a2b0837ef380722f6e05eae7890ffee61f0c1
Sudheer <sudheerlikeu@gmail.com>
2021-09-22 14:30:02 +0530

    auto pushing chainges repo

--- grafana/install.sh
+++ grafana/install.sh
@@ -2,5 +2,13 @@
 
 ##  Install on RPM-based Linux (CentOS, Fedora, OpenSuse, Red Hat)
 
+#Checking Root user
+if [ $(id -u) -ne 0 ]; then
+    echo -e "\e[31m✗  Your must be a root user to perform this command..\e[0m"
+    exit 1
+fi
+curl -s  https://raw.githubusercontent.com/sudheermuthyala/DevOps_tools/main/grafana/grafana.repo >/etc/yum.repos.d/grafana.repo
 
-curl  >/etc/yum.repos.d/grafana.repo
+yum install grafana -y
+systemctl enable grafana-server
+systemctl start grafana-server

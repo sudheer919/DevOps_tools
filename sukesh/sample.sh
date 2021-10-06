9c982e0b304eab79df1ce1ec027526ee842c9a3f
Sudheer <sudheerlikeu@gmail.com>
2021-10-06 16:17:44 +0530

    auto pushing chainges repo

--- sukesh/sample.sh
+++ sukesh/sample.sh
@@ -6,8 +6,16 @@ COMPONENT=$1
 
 if [ -z ${COMPONENT} ] ;then
     echo "Provide Instance Name"
-    exit   1
+    exit 1
 fi
+
+STATUS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=catalogue" |jq .Reservations[].Instances[].State.Name| xargs)
+
+if [ $STATUS == "running"] ; then
+    echo "Already $COMPONENT is Exist"
+    exit 2
+  fi
+
 
 PR_IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"/ /g' )
 

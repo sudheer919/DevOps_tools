71641fc6e3f9f98671c05c605f2a75e2f454eb1e
Sudheer <sudheerlikeu@gmail.com>
2021-10-06 13:14:03 +0530

    auto pushing chainges repo

---
+++ sukesh/README.sh
@@ -0,0 +1,1 @@
+aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name | grep running &>/dev/null
---
+++ sukesh/sample.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+LID=lt-07a997b45f50e990a
+LVER=1
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}

bec8ff0e3724c0e14e86493c0a18f1eaa2de4e84
Sudheer <sudheerlikeu@gmail.com>
2021-10-06 18:07:03 +0530

    auto pushing chainges repo

--- sukesh/README.sh
+++
@@ -1,4 +0,0 @@
-aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name | grep running &>/dev/null
-
-
-aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Instances[]

876154c9351d22dd4aa46318cab941b80841cf50
Sudheer <sudheerlikeu@gmail.com>
2021-10-06 14:28:48 +0530

    auto pushing chainges repo

--- sukesh/sample.sh
+++ sukesh/sample.sh
@@ -3,4 +3,4 @@
 LID=lt-07a997b45f50e990a
 LVER=1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} |jq .Instances[]."InstanceId"
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} |jq .Instances[].InstanceId

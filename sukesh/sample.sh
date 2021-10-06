57963e613ae12a527a0bd881df0f9acd3a1ad528
Sudheer <sudheerlikeu@gmail.com>
2021-10-06 15:05:40 +0530

    auto pushing chainges repo

--- sukesh/sample.sh
+++ sukesh/sample.sh
@@ -2,5 +2,13 @@
 
 LID=lt-07a997b45f50e990a
 LVER=1
+INSTANCE_NAME=$1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} |jq .Instances[].InstanceId
+if [ -z ${INSTANCE_NAME} ] ;then
+    echo "Provide Instance Name"
+    exit   1
+fi
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications 'ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}]' 'ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}]'|jq .Instances[].PrivateIpAddress
+
+

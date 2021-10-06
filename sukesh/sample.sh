716fed56a3ac4d0457a5c44866e348b6ba1df5c3
Sudheer <sudheerlikeu@gmail.com>
2021-10-06 16:59:34 +0530

    auto pushing chainges repo

--- sukesh/sample.sh
+++ sukesh/sample.sh
@@ -12,10 +12,9 @@ fi
 #aws ec2 describe-instances --filters "Name=tag:Name,Values=catalogue" |jq .Reservations[].Instances[].State.Name | grep "running" | xargs &>/dev/null
 INSTANCE_EXISTS=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${COMPONENT} | jq .Reservations[])
 
-
-if [ z "${INSTANCE_EXISTS}" -o "$STATE" == "terminated" ] ; then
-    echo "Already $COMPONENT is Exist"
-    exit 2
+if [  -z  "${INSTANCE_EXISTS}" -o "$STATE" == "terminated" ] ; then
+      echo "Already $COMPONENT is Exist"
+      exit 2
     else
       echo "creating instance"
 fi

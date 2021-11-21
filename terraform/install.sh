411ea7dfb10cb4496808546505b93e201d852464
sudheer <sudheerlikeu@gmail.com>
2021-11-21 10:34:46 +0530

    auto pushing chainges repo

--- terraform/install.sh
+++ terraform/install.sh
@@ -11,18 +11,20 @@ CheckRoot() {
 }
 exit_status() {
   if [ $? -eq 0 ]; then
-    echo -e "\e[33m SUCCESS\e[0m"
+    echo -e "\e[33m $1  SUCCESS\e[0m"
   fi
 }
 ######################
 #progream
 ######################
+
 CheckRoot
 sudo yum install unzip -y  &>>/dev/null
 URL=$(curl -s https://www.terraform.io/downloads.html | grep "64-bit" | grep "linux_amd64.zip" | awk -F '"' '{print $2}')
 FILE_NAME=$(echo $URL | awk -F / '{print $NF}')
 curl -s -L -J -O $URL
-unzip -o $FILE_NAME -d /bin
-exit_status
+echo -e "\e[35m Installing Terrform \e[0m"
+unzip -o $FILE_NAME -d /bin &>>/dev/null
+exit_status "Installing Terraform is : "
 
 

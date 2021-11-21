4162821a3f9c74bf5b22b482d47abf9626837920
sudheer <sudheerlikeu@gmail.com>
2021-11-21 10:27:20 +0530

    auto pushing chainges repo

--- terraform/install.sh
+++ terraform/install.sh
@@ -15,6 +15,8 @@ exit_status() {
   fi
 }
 ######################
+#progream
+######################
 CheckRoot
 sudo yum install unzip -y
 URL=$(curl https://www.terraform.io/downloads.html | grep "64-bit" | grep "linux_amd64.zip" | awk -F '"' '{print $2}')

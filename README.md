453479e4f2dff50d205a34886c65ce9b43725129
sudheer <sudheerlikeu@gmail.com>
2021-12-16 12:22:23 +0530

    auto pushing chainges repo

--- README.md
+++ README.md
@@ -13,8 +13,17 @@ alias stop='sudo systemctl stop'
 alias sd='sudo '
 alias n='sudo netstat -lntp'
 alias c='curl ifconfig.co'
+alias l='ls -la'
+alias p='git pull'
+alias ti='terraform init'
+alias tp='terraform plan'
+alias ta='terraform apply -auto-approve'
+alias td='terraform destroy -auto-approve'
 
-alias p='sudo git pull'
+alias 1='cd ../'
+alias 2='cd ../ ../'
+alias 3='cd ../ ../ ../'
+alias 4='cd ../ ../ ../ ../'
 alias t='sudo tail -f /var/log/messages'" >> /etc/profile.d/my_auto_script.sh && su -
 
 mkdir -R /home/centos/.ssh/
--- terraform/install.sh
+++ terraform/install.sh
@@ -19,12 +19,29 @@ exit_status() {
 ######################
 
 CheckRoot
-sudo yum install unzip -y  &>>/dev/null
-URL=$(curl -s https://www.terraform.io/downloads.html | grep "64-bit" | grep "linux_amd64.zip" | awk -F '"' '{print $2}')
-FILE_NAME=$(echo $URL | awk -F / '{print $NF}')
-curl -s -L -J -O $URL
-echo -e "\e[35m Installing Terrform \e[0m"
-unzip -o $FILE_NAME -d /bin &>>/dev/null
+#sudo yum install unzip -y  &>>/dev/null
+#URL=$(curl -s https://www.terraform.io/downloads.html | grep "64-bit" | grep "linux_amd64.zip" | awk -F '"' '{print $2}')
+#FILE_NAME=$(echo $URL | awk -F / '{print $NF}')
+#curl -s -L -J -O $URL
+#echo -e "\e[35m Installing Terrform \e[0m"
+#unzip -o $FILE_NAME -d /bin &>>/dev/null
+sudo yum install -y yum-utils &>>/dev/null
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo &>>/dev/null
+sudo yum -y install terraform &>>/dev/null
 exit_status "Installing Terraform is : "
+
+
+
+
+#git init
+#git add .
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/sudheermuthyala/reff-terraform.git
+#git push -u origin main
+#
+
+
+
 
 

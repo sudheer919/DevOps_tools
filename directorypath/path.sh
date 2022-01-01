08b788f9819f96bd8ef782e55b5ca48b7236644f
sudheer <sudheerlikeu@gmail.com>
2022-01-01 17:27:40 +0530

    auto pushing chainges repo

--- directorypath/path.sh
+++ directorypath/path.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-P="\e[35m"
-#PAT=`ls -lh $1`
+#R="\e[31m"
+#G="\e[32m"
+#N="\e[0m"
+#P="\e[35m"
+##PAT=`ls -lh $1`
+#
+#read -p "Enter path : " filep
+#for file in $(find $filep );
+#do
+#      if [ -d $file ]; then
+#          echo -e "$G$file is a directory$N"
+#     elif [ -f $file ]; then
+#           echo -e "$P$file is a file $N"
+#
+#      fi
+#done >file.txt
 
-read -p "Enter path : " filep
-for file in $(find $filep );
+
+
+#!/bin/bash
+read -p "Enter path : " -r filep
+echo " file path   -   size "
+for i in $(find "$filep" -depth);
 do
-      if [ -d $file ]; then
-          echo -e "$G$file is a directory$N"
-     elif [ -f $file ]; then
-           echo -e "$P$file is a file $N"
-#     else
-#           echo -e "R No file found N"
-      fi
-done >file.txt
-
-#for i in $(find "$filep" -depth);
+        size=$(stat -c%s "$i")
+        if [ $size -gt 1000 ]
+        then
+                echo $i " - " $size
+        fi
+done

0416887b126622b224061a141f354a0f505dcb2a
sudheer <sudheerlikeu@gmail.com>
2022-01-01 17:08:09 +0530

    auto pushing chainges repo

--- directorypath/path.sh
+++ directorypath/path.sh
@@ -5,5 +5,14 @@ N="\e[0m"
 P="\e[35m"
 #PAT=`ls -lh $1`
 
-read -p " Enter the path to Directory/file :"  PATH
-echo "$PATH"
+read -p "Enter path : " filep
+for file in $filep
+do
+      if [ -d $file ]; then
+          echo $file is a directory
+     elif [ -f $file ]; then
+           echo $file is a file
+     else
+           echo No file found
+      fi
+done >file.txt

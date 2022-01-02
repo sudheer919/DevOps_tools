280b5fd312b3d3314406919d15f30557f25f7d2d
sudheer <sudheerlikeu@gmail.com>
2022-01-02 16:19:15 +0530

    auto pushing chainges repo

--- directorypath/path.sh
+++ directorypath/path.sh
@@ -7,7 +7,7 @@ P="\e[35m"
 read -p "Enter path : " -r filep
 echo "Given Path is..[$filep]"
 echo "File/Directory - [file path]   -   [size]"
-echo 'Output is redirected to "outputfile.txt"'
+
 for i in $(find "$filep" -depth);
 do
          size=$(stat -c%s "$i")
@@ -19,4 +19,6 @@ do
                 echo   "$P File $N\t[$i]-\t[$R$size$N]"
         fi
 done >outputfile.txt
+
+echo 'Output is redirected to "outputfile.txt"'
 
